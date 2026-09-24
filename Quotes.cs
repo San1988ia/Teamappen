7c52a64db2a2b0a78f90817ce65034e2f362f656
Stephanie Baard <stephanie.baard@gmail.com>
2026-09-24 14:16:27 +0200

    ny qoutes

--- Quotes.cs
+++ Quotes.cs
@@ -7,6 +7,7 @@ static class Quotes
         "Commit early, commit often.",
         "Det fungerar på min dator.",
         "Strålande blå sol",
+        "hej babes",
     };
 
     public static string GetQuote()
