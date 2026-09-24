b7d17f509d510fc4fe29341a0f2e2324f2c3bcfc
jessicaagren <jessicaagren@hotmail.com>
2026-09-24 14:16:02 +0200

    Lägg till nytt citat i listan

--- Quotes.cs
+++ Quotes.cs
@@ -7,6 +7,7 @@ static class Quotes
         "Commit early, commit often.",
         "Det fungerar på min dator.",
         "Strålande blå sol",
+        "Roses are red, violets are blue, keep calm § and code on.",
     };
 
     public static string GetQuote()
