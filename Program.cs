bd0e0372d95bc09e39864e5d5edc7f2f0266d40f
Stephanie Baard <stephanie.baard@gmail.com>
2026-09-24 14:07:40 +0200

    ny text

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
         Console.WriteLine("De tre muskutörerna");
-        Console.WriteLine("Välkommen!");
+        Console.WriteLine("Välkommen Sania och Jessica!");
         Console.WriteLine();
 
         Team.PrintMembers();
