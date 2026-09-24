7058d8aceab396231836b05d8f387e3f354c9235
jessicaagren <jessicaagren@hotmail.com>
2026-09-24 14:07:18 +0200

    Ändra välkomsttext

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
         Console.WriteLine("De tre muskutörerna");
-        Console.WriteLine("Välkommen!");
+        Console.WriteLine("Tjolahopp!");
         Console.WriteLine();
 
         Team.PrintMembers();
